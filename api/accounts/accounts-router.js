ea330128ed5e82bae30ec80a37f7413ba5e04986
RealSadLemon <esmodearburk@gmail.com>
2022-07-18 18:18:26 -0500

    Set up functionality for GET all accounts

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,38 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const accountsModel = require('./accounts-model');
+const accountsMiddleware = require('./accounts-middleware');
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  accountsModel.getAll()
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  return
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  return
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  return
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  return
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  return
 })
 
 module.exports = router;
